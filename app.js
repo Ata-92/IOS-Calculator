cecf7d3df16278bec927206dc9e70b6a9053b528
Ata-92 <atakal92@gmail.com>
2021-04-08 02:25:56 +0300

    Add two more buttons events

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ buttons[0].addEventListener("click", () => {
   lineDown.textContent = 0;
 });
 
+let counter = 0;
+buttons[1].addEventListener("click", () => {
+  counter++;
+  if (counter % 2 == 1 && lineDown.textContent != 0) {
+    lineDown.textContent = "-" + lineDown.textContent;
+  } else {
+    lineDown.textContent = lineDown.textContent;
+  }
+});
+
+buttons[2].addEventListener("click", () => {
+  if (lineDown.textContent.includes("-") && lineDown.textContent.length == 2) {
+    lineDown.textContent = "";
+  } else {
+    lineDown.textContent = lineDown.textContent.slice(0, -1);
+  }
+});
