6ab4ac9962daa11127fe9cb0eb66f19a7c8f20a7
Ata-92 <atakal92@gmail.com>
2021-04-08 02:24:45 +0300

    Add first event listener

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const lineUp = document.querySelector(".up");
 const lineDown = document.querySelector(".down");
 const buttons = document.querySelectorAll("button");
 
-
+buttons[0].addEventListener("click", () => {
+  lineUp.textContent = "";
+  lineDown.textContent = 0;
+});
 
